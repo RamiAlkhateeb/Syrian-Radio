aa055ec48cbd3251c8cbda9a7b90706bbf6ce241
Rami Alkhateeb <rami13195@gmail.com>
2026-08-30 16:36:38 +0200

    Add Cloudflare Pages build configuration

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -eu
+
+curl -sSL https://dot.net/v1/dotnet-install.sh -o dotnet-install.sh
+chmod +x dotnet-install.sh
+./dotnet-install.sh -c 9.0 -InstallDir ./dotnet
+./dotnet/dotnet publish -c Release -o output
